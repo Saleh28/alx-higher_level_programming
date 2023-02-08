94add9d97f2cfac8f09151c5d7a3e9eb03abe0ec
saleh28 <abubakarsaleh702@gmail.com>
2023-02-08 15:45:50 -0800

    t3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module for json string
+    """
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ json representation of object
+    """
+    return json.dumps(my_obj)
+
